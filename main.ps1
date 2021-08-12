6799e94701618e70430761102bfe16bce8348a4c
Simon Nyvall <simon.k.nyvall@gmail.com>
2021-08-12 14:48:54 +0200

    Added delete function

--- main.ps1
+++ main.ps1
@@ -1,15 +1,22 @@
-
 # Variables
-$files       = Get-ChildItem "/home/simon/Desktop/test"
-$username    =               "ubuntu"
-$ip_address  =               "192.168.1.229"
-$destination =               "/home/ubuntu/NAS/ssh-file-transfer-folder/"
+$send_folder_path = "/home/simon/Desktop/Server-file-transfer"
+$files = Get-ChildItem $send_folder_path
+$username = "ubuntu"
+$ip_address = "192.168.1.229"
+$destination = "/home/ubuntu/NAS/ssh-file-transfer-folder/"
 function main {
 
     # Command To transfer files
     for ($i = 0; $i -lt $files.Count; $i++) {
-        iex $command = ("scp " + [string]$files[$i] + " " + $username + "@" + $ip_address + ":" + $destination)
+        $SCP_command = ("scp " + [string]$files[$i] + " " + $username + "@" + $ip_address + ":" + $destination)
+        iex $SCP_command
     }
+    # Deletes the transferd files
+    $cd_command = ("cd " + $send_folder_path)
+    iex $cd_command
+
+    $delete_command = "rm *"
+    iex $delete_command
 }
 
 main;
